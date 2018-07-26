4fb6ce5329c6f9403f7c36891add1bc60427301a
zzna <jjuna95@naver.com>
2018-07-26 22:24:48 +0900

    header profile view 완성!

--- LaundryDay/Views/HeaderProfileCollectionReusableView.swift
+++ LaundryDay/Views/HeaderProfileCollectionReusableView.swift
@@ -23,8 +23,16 @@ class HeaderProfileCollectionReusableView: UICollectionReusableView {
         }
     }
     func updateView(){
-        self.nameLabel.text=user!.userName
-        if let photoUrlString = user!.profileImageUrl{
+        //옵셔널체이닝 검색해서 적용해봄. fatal error
+        //그런데 모르겠어서 그냥 !만 ?로 바꿨는데 해결됨...
+        
+        //if let self.nameLabel.text=user?.userName{
+        //    print(self.nameLabel.text)
+        //}
+        self.nameLabel.text=user?.userName
+        
+        //이것도 user!했더니 nil이라 언래핑 오류래서 그냥 ?만 바꿨는데 해결됐다..
+        if let photoUrlString = user?.profileImageUrl{
             let photoUrl = URL(string: photoUrlString)
             self.profileImage.sd_setImage(with: photoUrl)
             
