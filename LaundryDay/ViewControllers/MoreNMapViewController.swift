0fe0483e53d5b4c46bda19198515c504271fe8da
zzna <jjuna95@naver.com>
2018-07-29 22:59:29 +0900

    남은 것 : 기본 상태가 세탁방 검색상태인 것, 네이버 지도 어플 연결

--- LaundryDay/ViewControllers/MoreNMapViewController.swift
+++ LaundryDay/ViewControllers/MoreNMapViewController.swift
@@ -8,9 +8,69 @@
 
 import UIKit
 
-class MoreNMapViewController: UIViewController,NMapViewDelegate, NMapPOIdataOverlayDelegate {
+//nil이랑 (x:0, y:0) 주석처리하고 원래 코드 살리기 [MARK: - NMapPOIdataOverlayDelegate]
+//NMapViewResources.swift
+
+// set custom POI Flag Type.
+// NMapPOIflagTypeReserved 보다 큰 값으로 설정.
+let UserPOIflagTypeDefault: NMapPOIflagType = NMapPOIflagTypeReserved + 1
+let UserPOIflagTypeInvisible: NMapPOIflagType = NMapPOIflagTypeReserved + 2
+
+class NMapViewResources: NSObject {
+    
+    open static func imageWithType(_ poiFlagType: NMapPOIflagType, selected: Bool) -> UIImage? {
+        switch poiFlagType {
+        case NMapPOIflagTypeLocation:
+            return #imageLiteral(resourceName: "pubtrans_ic_mylocation_on")
+        case NMapPOIflagTypeLocationOff:
+            return #imageLiteral(resourceName: "pubtrans_ic_mylocation_off")
+        case NMapPOIflagTypeCompass:
+            return #imageLiteral(resourceName: "ic_angle")
+        case UserPOIflagTypeDefault:
+            return #imageLiteral(resourceName: "pubtrans_exact_default")
+        case UserPOIflagTypeInvisible:
+            return #imageLiteral(resourceName: "1px_dot")
+        default:
+            return nil
+        }
+    }
+    
+    open static func anchorPoint(withType type: NMapPOIflagType) -> CGPoint {
+        switch type {
+        case NMapPOIflagTypeLocation: fallthrough
+        case NMapPOIflagTypeLocationOff: fallthrough
+        case NMapPOIflagTypeCompass:
+            return CGPoint(x: 0.5, y: 0.5)
+        case UserPOIflagTypeDefault:
+            return CGPoint(x: 0.5, y: 1.0)
+        case UserPOIflagTypeInvisible:
+            return CGPoint(x: 0.5, y: 0.5)
+        default:
+            return CGPoint(x: 0.5, y: 0.5)
+        }
+    }
+}
+
+//본 코드 시작
+
+class MoreNMapViewController: UIViewController,NMapViewDelegate, NMapPOIdataOverlayDelegate, NMapLocationManagerDelegate {
     
     var mapView: NMapView?
+    
+
+    
+    
+    //현재위치
+    var changeStateButton: UIButton?
+    
+    enum state {
+        case disabled
+        case tracking
+        case trackingWithHeading
+    }
+    
+    var currentState: state = .disabled
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView = NMapView(frame: self.view.frame)
@@ -26,7 +86,70 @@ class MoreNMapViewController: UIViewController,NMapViewDelegate, NMapPOIdataOver
             
             view.addSubview(mapView)
         }
+        
+        //현재위치 표시하기
+        changeStateButton = createButton()
+        
+        if let button = changeStateButton {
+            view.addSubview(button)
+        }
     }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        if view.frame.size.width != size.width {
+            if let mapView = mapView, mapView.isAutoRotateEnabled {
+                mapView.setAutoRotateEnabled(false, animate: false)
+                
+                coordinator.animate(alongsideTransition: {(context: UIViewControllerTransitionCoordinatorContext) -> Void in
+                    if let mapView = self.mapView {
+                        mapView.setAutoRotateEnabled(true, animate: false)
+                    }
+                }, completion: nil)
+                
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        mapView?.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        mapView?.viewWillAppear()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        mapView?.viewDidDisappear()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    open func onMapView(_ mapView: NMapView!, initHandler error: NMapError!) {
+        if (error == nil) { // success
+            // set map center and level
+            mapView.setMapCenter(NGeoPoint(longitude:126.978371, latitude:37.5666091), atLevel:11)
+            // set for retina display
+            mapView.setMapEnlarged(true, mapHD: true)
+            // set map mode : vector/satelite/hybrid
+            mapView.mapViewMode = .vector
+        } else { // fail
+            print("onMapView:initHandler: \(error.description)")
+        }
+    }
+    
+    /*
     public func onMapView(_ mapView: NMapView!, initHandler error: NMapError!) {
         if (error == nil) { // success
             // set map center and level
@@ -37,27 +160,108 @@ class MoreNMapViewController: UIViewController,NMapViewDelegate, NMapPOIdataOver
         } else { // fail
             print("onMapView:initHandler: \(error.description)")
         }
+    }*/
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        mapView?.viewDidAppear()
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        mapView?.viewWillDisappear()
+        
+        stopLocationUpdating()
+    }
+
+    
     
     // MARK: - NMapPOIdataOverlayDelegate
     
     open func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, imageForOverlayItem poiItem: NMapPOIitem!, selected: Bool) -> UIImage! {
-        return nil
+        return NMapViewResources.imageWithType(poiItem.poiFlagType, selected: selected) //nil
     }
     
     open func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, anchorPointWithType poiFlagType: NMapPOIflagType) -> CGPoint {
-        return CGPoint(x: 0, y: 0)
+        return NMapViewResources.anchorPoint(withType: poiFlagType) //CGPoint(x: 0, y: 0)
     }
     
     open func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, calloutOffsetWithType poiFlagType: NMapPOIflagType) -> CGPoint {
-        return CGPoint(x: 0, y: 0)
+        return CGPoint.zero //CGPoint(x: 0, y: 0)
     }
     
     open func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, imageForCalloutOverlayItem poiItem: NMapPOIitem!, constraintSize: CGSize, selected: Bool, imageForCalloutRightAccessory: UIImage!, calloutPosition: UnsafeMutablePointer<CGPoint>!, calloutHit calloutHitRect: UnsafeMutablePointer<CGRect>!) -> UIImage! {
         return nil
     }
     
+    // MARK: - NMapLocationManagerDelegate Methods
     
+    open func locationManager(_ locationManager: NMapLocationManager!, didUpdateTo location: CLLocation!) {
+        
+        let coordinate = location.coordinate
+        
+        let myLocation = NGeoPoint(longitude: coordinate.longitude, latitude: coordinate.latitude)
+        let locationAccuracy = Float(location.horizontalAccuracy)
+        
+        mapView?.mapOverlayManager.setMyLocation(myLocation, locationAccuracy: locationAccuracy)
+        mapView?.setMapCenter(myLocation)
+    }
+    
+    open func locationManager(_ locationManager: NMapLocationManager!, didFailWithError errorType: NMapLocationManagerErrorType) {
+        
+        var message: String = ""
+        
+        switch errorType {
+        case .unknown: fallthrough
+        case .canceled: fallthrough
+        case .timeout:
+            message = "일시적으로 내위치를 확인 할 수 없습니다."
+        case .denied:
+            message = "위치 정보를 확인 할 수 없습니다.\n사용자의 위치 정보를 확인하도록 허용하시려면 위치서비스를 켜십시오."
+        case .unavailableArea:
+            message = "현재 위치는 지도내에 표시할 수 없습니다."
+        case .heading:
+            message = "나침반 정보를 확인 할 수 없습니다."
+        }
+        
+        if (!message.isEmpty) {
+            let alert = UIAlertController(title:"NMapViewer", message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title:"OK", style:.default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+        
+        if let mapView = mapView, mapView.isAutoRotateEnabled {
+            mapView.setAutoRotateEnabled(false, animate: true)
+        }
+    }
+    
+    func locationManager(_ locationManager: NMapLocationManager!, didUpdate heading: CLHeading!) {
+        let headingValue = heading.trueHeading < 0.0 ? heading.magneticHeading : heading.trueHeading
+        setCompassHeadingValue(headingValue)
+    }
+    
+    func onMapViewIsGPSTracking(_ mapView: NMapView!) -> Bool {
+        return NMapLocationManager.getSharedInstance().isTrackingEnabled()
+    }
+    
+    func findCurrentLocation() {
+        enableLocationUpdate()
+    }
+    
+    func setCompassHeadingValue(_ headingValue: Double) {
+        
+        if let mapView = mapView, mapView.isAutoRotateEnabled {
+            mapView.setRotateAngle(Float(headingValue), animate: true)
+        }
+    }
+    
+    func stopLocationUpdating() {
+        
+        disableHeading()
+        disableLocationUpdate()
+    }
     
     
     //일단 아이콘 붙여둠
@@ -77,16 +281,155 @@ class MoreNMapViewController: UIViewController,NMapViewDelegate, NMapPOIdataOver
             return nil
         }
     }
-
     
     
     
+    // MARK: - My Location
     
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func enableLocationUpdate() {
+        
+        if let lm = NMapLocationManager.getSharedInstance() {
+            
+            if lm.locationServiceEnabled() == false {
+                locationManager(lm, didFailWithError: .denied)
+                return
+            }
+            
+            if lm.isUpdateLocationStarted() == false {
+                // set delegate
+                lm.setDelegate(self)
+                // start updating location
+                lm.startContinuousLocationInfo()
+            }
+        }
     }
+    
+    func disableLocationUpdate() {
+        
+        if let lm = NMapLocationManager.getSharedInstance() {
+            
+            if lm.isUpdateLocationStarted() {
+                // start updating location
+                lm.stopUpdateLocationInfo()
+                // set delegate
+                lm.setDelegate(nil)
+            }
+        }
+        
+        mapView?.mapOverlayManager.clearMyLocationOverlay()
+    }
+    
+    
+    // MARK: - Compass
+    
+    func enableHeading() -> Bool {
+        
+        if let lm = NMapLocationManager.getSharedInstance() {
+            
+            let isAvailableCompass = lm.headingAvailable()
+            
+            if isAvailableCompass {
+                
+                mapView?.setAutoRotateEnabled(true, animate: true)
+                
+                lm.startUpdatingHeading()
+            } else {
+                return false
+            }
+        }
+        
+        return true;
+    }
+    
+    func disableHeading() {
+        if let lm = NMapLocationManager.getSharedInstance() {
+            
+            let isAvailableCompass = lm.headingAvailable()
+            
+            if isAvailableCompass {
+                lm.stopUpdatingHeading()
+            }
+        }
+        
+        mapView?.setAutoRotateEnabled(false, animate: true)
+    }
+    
+    // MARK: - Button Control
+    
+    func createButton() -> UIButton? {
+        
+        let button = UIButton(type: .custom)
+        
+        button.frame = CGRect(x: 15, y: 30, width: 36, height: 36)
+        button.setImage(#imageLiteral(resourceName: "v4_btn_navi_location_normal"), for: .normal)
+        
+        button.addTarget(self, action: #selector(buttonClicked(_:)), for: .touchUpInside)
+        
+        //button.addTarget(self, action: #selector(buttonClicked(_:)), for: .touchUpInside)
+        
+        return button
+    }
+    
+    @objc func buttonClicked(_ sender: UIButton!) {
+        
+        if let lm = NMapLocationManager.getSharedInstance() {
+            
+            switch currentState {
+            case .disabled:
+                enableLocationUpdate()
+                updateState(.tracking)
+            case .tracking:
+                let isAvailableCompass = lm.headingAvailable()
+                
+                if isAvailableCompass {
+                    enableLocationUpdate()
+                    if enableHeading() {
+                        updateState(.trackingWithHeading)
+                    }
+                } else {
+                    stopLocationUpdating()
+                    updateState(.disabled)
+                }
+            case .trackingWithHeading:
+                stopLocationUpdating()
+                updateState(.disabled)
+            }
+        }
+    }
+    
+    func updateState(_ newState: state) {
+        
+        currentState = newState
+        
+        switch currentState {
+        case .disabled:
+            changeStateButton?.setImage(#imageLiteral(resourceName: "v4_btn_navi_location_normal"), for: .normal)
+        case .tracking:
+            changeStateButton?.setImage(#imageLiteral(resourceName: "v4_btn_navi_location_selected"), for: .normal)
+        case .trackingWithHeading:
+            changeStateButton?.setImage(#imageLiteral(resourceName: "v4_btn_navi_location_my"), for: .normal)
+        }
+    }
+    
+    /*
+    //현재위치 이동하기
+    if let lm = NMapLocationManager.getSharedInstance() {
+        // set delegate
+        lm.setDelegate(self)
+        
+        // start updating location
+        lm.startContinuousLocationInfo()
+        
+        mapView?.setAutoRotateEnabled(true, animate: true)
+        
+        // start updating heading
+        lm.startUpdatingHeading()
+    } 
+    
+    
+ */
+
+   
     
 
     /*
