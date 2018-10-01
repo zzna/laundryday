570680a6245b0dbf8649b2c33d381ae3947731aa
MinJeongChoKO <mjchokorea@gmail.com>
2018-10-01 19:12:05 +0900

    세탁 기호 VC추가

---
+++ LaundryDay/ViewControllers/WashingSymbolViewController.swift
@@ -0,0 +1,32 @@
+//
+//  WashingSymbolViewController.swift
+//  LaundryDay
+//
+//  Created by mjcho on 2018. 10. 1..
+//  Copyright © 2018년 MBP03. All rights reserved.
+//
+
+import UIKit
+
+class WashingSymbolViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+
+}
+
+extension WashingSymbolViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WashingSymbolTableViewCell", for: indexPath) as! WashingSymbolTableViewCell
+        return cell
+    }
+    
+}
---
+++ LaundryDay/Views/WashingSymbolTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WashingSymbolTableViewCell.swift
+//  LaundryDay
+//
+//  Created by mjcho on 2018. 10. 1..
+//  Copyright © 2018년 MBP03. All rights reserved.
+//
+
+import UIKit
+
+class WashingSymbolTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
